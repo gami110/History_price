d3112e0860cd42877e0728d532ca664f8d199699
Salavat <gamespay110@gmail.com>
2024-02-29 23:31:59 +0500

    formating

--- Parsing/parsing_product.py
+++ Parsing/parsing_product.py
@@ -2,8 +2,7 @@ import undetected_chromedriver as uc
 from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support import expected_conditions as ec
 from database.job_database import get_links_from_db
 
 
@@ -12,14 +11,14 @@ def parse_product_data_selenium():
     options = uc.ChromeOptions()
     options.headless = False
     driver = uc.Chrome(options=options)
-    wait = WebDriverWait(driver, 10)  # ждем до 10 секунд
+    wait = WebDriverWait(driver, 10)
 
     products_data = []
     for url in urls:
         driver.get(url)
 
         try:
-            product_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-id]')))
+            product_element = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'div[data-id]')))
 
             product_id = product_element.get_attribute('data-id')
             product_name = product_element.get_attribute('data-name')
